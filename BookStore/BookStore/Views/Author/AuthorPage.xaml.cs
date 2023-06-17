860298dfe242c2f6f0e28cb91dea76e98da9cd53
EdytaRz <edytarzonc@gmail.com>
2023-06-17 17:23:44 +0200

    Add AuthorPage.xaml.cs

--- BookStore/BookStore/Views/Author/AuthorPage.xaml.cs
+++ BookStore/BookStore/Views/Author/AuthorPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BookStore.ViewModels;
+using BookStore.ViewModels.Author;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,9 +14,18 @@ namespace BookStore.Views.Author
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AuthorPage : ContentPage
     {
+        private AuthorViewModel _viewModel;
+
         public AuthorPage()
         {
             InitializeComponent();
+            BindingContext = _viewModel = new AuthorViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
         }
     }
 }
