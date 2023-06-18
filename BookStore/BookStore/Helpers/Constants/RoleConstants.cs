c0a3560735436742d4d92bffeef8ec1da64c8aad
Klaudia Kurela <klakure97@gmail.com>
2023-06-18 17:11:46 +0200

    Added RoleConstants

---
+++ BookStore/BookStore/Helpers/Constants/RoleConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Helpers.Constants
+{
+    public static class RoleConstants
+    {
+        public static readonly string UserRole = "UserRole";
+        public static readonly string Admin = "Admin";
+        public static readonly string User = "User";
+
+        public static List<string> GetRoles()
+        {
+            return new List<string> { Admin, User };
+        }
+    }
+}
